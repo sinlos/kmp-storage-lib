bbf2ac43632ffa394c16dbb39a4de8ac4bd5ba20
Ilya V. Pogrebenko <sinlos@gmail.com>
2026-03-02 11:11:07 +0300

    CI fix #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
+
 group = "world.chebur.kmp"
 version = findProperty("version")?.toString() ?: "1.0.0"
 
@@ -86,5 +88,21 @@ publishing {
         groupId = "world.chebur.kmp"
         artifactId = "kmp-storage"
         version = findProperty("version")?.toString() ?: "1.0.0"
+    }
+}
+
+// Отключаем публикацию target-specific артефактов в GitHub Packages
+// Публикуем только kotlinMultiplatform
+tasks.withType<PublishToMavenRepository> {
+    val publicationName = publication.name
+    val repositoryName = repository.name
+
+    // Пропускаем публикацию всех, кроме kotlinMultiplatform в GitHubPackages
+    onlyIf {
+        if (repositoryName == "GitHubPackages") {
+            publicationName == "kotlinMultiplatform"
+        } else {
+            true
+        }
     }
 }
